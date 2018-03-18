3260d744550aca1c45a48d53f4cc84ebeb69f983
Ralph Niemitz <ralph.niemitz@gmx.de>
2018-03-18 17:06:00 +0100

    18.03.2018
    Signed-off-by: Ralph Niemitz <ralph.niemitz@gmx.de>

--- src/test/java/de/ralleytn/simple/input/tests/KeyboardTest.java
+++ src/test/java/de/ralleytn/simple/input/tests/KeyboardTest.java
@@ -29,9 +29,5 @@ import org.junit.jupiter.api.Test;
 
 class KeyboardTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+	
 }
--- src/test/java/de/ralleytn/simple/input/tests/MouseTest.java
+++ src/test/java/de/ralleytn/simple/input/tests/MouseTest.java
@@ -29,9 +29,5 @@ import org.junit.jupiter.api.Test;
 
 class MouseTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
 }
